a425359414a6569aefdd558cf52ffcd79d6eab6c
Alpay Aydıngüler <alpayaydinguler@gmail.com>
2024-10-27 15:02:59 +0300

    Add project files.

---
+++ Architectural Ideas Contest/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Architectural_Ideas_Contest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
